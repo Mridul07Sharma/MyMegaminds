78893e1fbc1778c724f648080e5177484a7fb666
tejas1113 <tejasmore1104@gmail.com>
2022-05-27 13:49:04 +0530

    Commit successful

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+
+import Login from "./Components/Forms/Login";
+import Student from "./Components/Forms/Student";
+import Tutor from "./Components/Forms/Tutor";
+import Signup from "./Components/Signup";
+import About from "./Components/Pages/About";
+import Careers from "./Components/Pages/Careers";
+import Blog from "./Components/Pages/Blog";
+import Faq from "./Components/Pages/Faq.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header />
+              <Home />
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup/student" element={<Student />} />
+        <Route path="/signup/tutor" element={<Tutor />} />
+        <Route
+          path="/about"
+          element={
+            <div>
+              <About />
+            </div>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <div>
+              <Header />
+              <Blog />
+              <Footer />
+            </div>
+          }
+        />
+        <Route
+          path="/careers"
+          element={
+            <div>
+              <Header />
+              <Careers />
+              <Footer />
+            </div>
+          }
+        />
+        <Route
+          path="/faq"
+          element={
+            <div>
+              <Header />
+              <Faq />
+              <Footer />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
